3d6bcf074e1dbe4f6b96cb1854104e002a76083d
Diamond GC <gcdiamond789@gmail.com>
2023-11-04 23:39:04 -0500

    working on professor

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,12 +4,15 @@ import "./index.css"
 import Login from "../src/pages/Login.jsx";
 import Professor_SignUp from "./pages/Professor_SignUp.jsx";
 import Student_SignUp from "./pages/Student_SignUp.jsx";
+import Prof_Profile from "./pages/Prof_Profile.jsx";
+
+
 import SignUp from "../src/pages/SignUp.jsx";
 function App() {
 
   return (
     <>
-      <Student_SignUp />
+      <Prof_Profile />
     </>
   );
 }
---
+++ Client/src/pages/Prof_Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Prof_Profile() {
+    return (
+        <div className="Background">
+          <div className="Prof_Profile_Container">
+            <img src="" alt="Description of the image">
+          <p></p>
+          </div>
+        </div>
+    );
+}
+
+export default Prof_Profile
