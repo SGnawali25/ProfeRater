2f715a5cf4937582d8630218c1c877ad7a7da65d
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-11-04 22:45:12 -0500

    Rating Component Done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "../src/pages/Login.jsx";
 import SignUp from "../src/pages/SignUp.jsx";
+import Rating from "../src/components/Rating.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <SignUp />
+      <Rating />
     </>
   );
 }
