f3ac02988cf5e5c898bd15d2b13b50999beba9a7
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-11-04 19:11:39 -0500

    Login Page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "../src/pages/Login.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
 export default App;
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Login() {
-  return <div></div>;
+  return (
+    <div className="Background">
+      <div className="Login_Container">
+        <h1 className="Login_Text">Login</h1>
+        <form>
+          <input className="textbox" type="text" placeholder=".edu email" />
+          <input className="textbox" type="password" placeholder="Password" />
+          <button className="Login_Button" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+      <h6>Don't have an account?</h6>
+      <button className="SignUp_Button" type="submit">
+        Sign Up
+      </button>
+    </div>
+  );
 }
 
 export default Login;
