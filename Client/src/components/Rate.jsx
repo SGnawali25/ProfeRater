393eb70490ef9c5f5cf137cba8855d5971da02e7
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-11-05 03:40:09 -0600

    Potential Full Submission

--- Client/src/components/Rate.jsx
+++ Client/src/components/Rate.jsx
@@ -3,12 +3,17 @@ import React from "react";
 function Rate() {
   return (
     <div>
-      <div className="Rate_Left">
-        <div>4.0</div>
-      </div>
-      <div className="Rate_Right">
-        <div className="std_name">Student Name</div>
-        <div className="Comment"> Loved the Professor</div>
+      <div className="Rate_Container">
+        <div className="Rate_Left">
+          <div>4.0</div>
+        </div>
+        <div className="Rate_Right">
+          <div className="std_name">Student Name</div>
+          <div className="Comment">
+            {" "}
+            Loved the Professor!!! Gives us enough time to work and prepare!!
+          </div>
+        </div>
       </div>
     </div>
   );
--- Client/src/pages/Student_Dashboard.jsx
+++ Client/src/pages/Student_Dashboard.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import Rating from "../components/Rating.jsx";
+import Rate from "../components/Rate.jsx";
 
 function Student_Dashboard() {
   return (
     <div>
-      <div className="Student_Dash_Left">
-        <h1>Professor Name</h1>
-        <h1>University Name</h1>
-        <button className="Rate_My_Prof" type="submit">
-          Rate Professor
-        </button>
+      <div className="Dash_Manager">
+        <div className="Student_Dash_Container">
+          <div className="Student_Dash_Left">
+            <h1>Professor Name</h1>
+            <h1>University Name</h1>
+            <button className="Rate_My_Prof" type="submit">
+              Rate Professor
+            </button>
+          </div>
+          <div className="Student_Dash_Right">
+            <Rating />
+          </div>
+        </div>
+        <div className="Ratings_Count">
+          <h1>30 ratings</h1>
+        </div>
+        <div className="Student_Dash_Main">
+          <Rate />
+        </div>
       </div>
-      <div className="Student_Dash_Right">
-        <Rating />
-      </div>
-
-      <div className="Student_Dash_Main"></div>
     </div>
   );
 }
