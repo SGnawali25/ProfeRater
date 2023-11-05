102aba28df471082dd249d790d5579069a7e5c01
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-11-04 23:36:52 -0500

    Mid Progress

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,17 @@
 import { useState } from "react";
 import "./App.css";
-import "./index.css"
+import "./index.css";
 import Login from "../src/pages/Login.jsx";
 import Professor_SignUp from "./pages/Professor_SignUp.jsx";
 import Student_SignUp from "./pages/Student_SignUp.jsx";
 import SignUp from "../src/pages/SignUp.jsx";
-<<<<<<< HEAD
 import Rating from "../src/components/Rating.jsx";
+import Professor_Dashboard from "./pages/Professor_Dashboard";
 
-=======
->>>>>>> 3b3bb9f96fa45d27b86274dd94c0a79d7ede1024
 function App() {
-
   return (
     <>
-<<<<<<< HEAD
-      <Rating />
-=======
-      <Student_SignUp />
->>>>>>> 3b3bb9f96fa45d27b86274dd94c0a79d7ede1024
+      <Professor_Dashboard />
     </>
   );
 }
---
+++ Client/src/pages/Professor_Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Professor_Dashboard() {
+  return (
+    <div>
+      <div className="Professor_Info">
+        <div className="image_footer">
+          <h1>Professor Name</h1>
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Professor_Dashboard;
