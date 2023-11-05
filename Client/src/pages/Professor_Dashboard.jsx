468ea531860e452057faf7f881bddcdbf70f5cf3
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-11-05 00:57:00 -0500

    Professor_Dashboard complete

--- Client/src/pages/Professor_Dashboard.jsx
+++ Client/src/pages/Professor_Dashboard.jsx
@@ -1,12 +1,43 @@
 import React from "react";
+import Rating from "../components/Rating.jsx";
 
 function Professor_Dashboard() {
   return (
     <div>
       <div className="Professor_Info">
         <div className="image_footer">
-          <h1>Professor Name</h1>
-          <h1></h1>
+          <div className="Prof_Line">
+            <h1 className="Prof_name">Professor Name</h1>
+            <h1 className="Prof_name">Department of Computer Science</h1>
+          </div>
+          <div className="University_Line">
+            <h3 className="Uni_name">University Name</h3>
+            <div className="rating">
+              <div className="star filled">&#9733;</div>
+              <div className="star filled">&#9733;</div>
+              <div className="star filled">&#9733;</div>
+              <div className="star filled">&#9733;</div>
+              <div className="star half-filled">&#9733;</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="Rating_Name"> Overall Rating</h1>
+      <div className="Dashboard">
+        <div className="Dashboard_Left">
+          <a href="#" className="text">
+            Edit Profile
+          </a>
+          <div className="dropdown">
+            <span>Courses</span>
+            <div className="dropdown-content">
+              <a href="#">CSCI 110</a>
+              <a href="#">CSCI 390</a>
+            </div>
+          </div>
+        </div>
+        <div className="Dashboard_Right">
+          <Rating />
         </div>
       </div>
     </div>
