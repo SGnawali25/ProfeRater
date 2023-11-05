fb1be65593c8af6e755d5fda18f0c3315b21b461
Diamond GC <gcdiamond789@gmail.com>
2023-11-04 20:17:58 -0500

    professor signup page complete

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "./index.css"
 import Login from "../src/pages/Login.jsx";
+import Student_SignUp from "../src/pages/Student_SignUp.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Login />
+      <Student_SignUp />
     </>
   );
 }
